033624b8a4a8054a724e64427a675e2bee306aca
safae <m692706@gmail.com>
2026-01-18 05:46:52 -0800

    Initial Commit

---
+++ calcul/calculApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculApp.swift
+//  calcul
+//
+//  Created by safae machmoum on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct calculApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
